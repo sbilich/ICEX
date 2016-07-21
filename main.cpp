2e0b2c54a2f4a5e748f4056486db1998424ce91d
Ian Dunn <iondune@gmail.com>
2016-07-21 01:35:45 -0700

    Increase size of points

--- main.cpp
+++ main.cpp
@@ -549,6 +549,7 @@ static void init()
    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
    glEnable(GL_CULL_FACE);
    glCullFace(GL_BACK);
+   glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
 
    // Added for ICEX
    // Generate framebuffer, texture, and depth renderbuffer
